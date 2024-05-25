566610f586c4d6d0b633378800bbbf5b9e400897
Zouuabi <zouabifoued8@gmail.com>
2024-05-25 14:50:23 +0100

    CORS Allow All

--- api/routes/_middleware.dart
+++ api/routes/_middleware.dart
@@ -3,12 +3,12 @@ import 'package:shelf_cors_headers/shelf_cors_headers.dart' as shelf;
 
 Handler middleware(Handler handler) {
   return handler.use(requestLogger()).use(
-        fromShelfMiddleware(
-          shelf.corsHeaders(
-            headers: {
-              shelf.ACCESS_CONTROL_ALLOW_ORIGIN: 'https://pfa.globeapp.dev',
-            },
-          ),
-        ),
-      );
+    fromShelfMiddleware(
+      shelf.corsHeaders(
+        originChecker: (origin) {
+          return shelf.originAllowAll(origin);
+        },
+      ),
+    ),
+  );
 }
